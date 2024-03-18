925b63d44b224721b3c99fc8749fe00903a0fef4
zondimaqina <zondimaqina0@gmail.com>
2024-03-18 12:39:03 +0200

    add pseudocode

---
+++ main.js
@@ -0,0 +1,12 @@
+// user press "start sketching"
+// window pops up asking for amount of grids
+// amount of grids appear on page
+// as user hovers on grids color appears 
+// color is permanent on each grid 
+
+
+// use arrays and loops for making grids appear
+// use mouseover eventlistener for color generator
+// store amount of colors in array
+
+
